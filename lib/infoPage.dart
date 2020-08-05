d8a2a2e6fb592a8287984bd4f17b5a29b4e10a95
Michaelmvh <mmvandenheuv@wisc.edu>
2020-08-04 23:08:41 -0500

    Added initial info page widgets

--- lib/infoPage.dart
+++ lib/infoPage.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
 import 'mappage.dart';
 
-class Bar extends StatelessWidget {
-  final String data;
-
-  Bar({this.data});
+class InfoPage extends StatelessWidget {
+  final String dormName;
+  InfoPage({Key key, @required this.dormName}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Text(data);
+    return Scaffold(
+        appBar: AppBar(
+          title: new Text("data"),
+        ),
+        body: Stack(
+          children: <Widget>[
+            new Image.network(//replace with URL from db
+                'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png'),
+            roomButtons(3) //room num from db,
+          ],
+        ));
+  }
+
+  ButtonBar roomButtons(int numRooms) {
+    List<Widget> buttons = [];
+    for (int i = 0; i < numRooms; i++) {
+      buttons.add(new FlatButton());
+    }
+    return ButtonBar(
+      alignment: MainAxisAlignment.center,
+      children: buttons,
+    );
   }
 }
