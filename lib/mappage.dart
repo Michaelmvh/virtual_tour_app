4fff27933566413561920d9213aca6b4182c25e5
Michaelmvh <mmvandenheuv@wisc.edu>
2020-08-07 22:34:05 -0500

    Added Info Window when pressing marker. Allows option to open information page

--- lib/mappage.dart
+++ lib/mappage.dart
@@ -3,16 +3,23 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-//import 'infoPage.dart';
-//import 'infoPill.dart';
+import 'infoPage.dart';
 
-class MapPage extends StatelessWidget {
+class MapPage extends StatefulWidget {
   final String campusName;
   MapPage({Key key, @required this.campusName}) : super(key: key);
+  @override
+  State<StatefulWidget> createState() => MapPageState(campusName);
+}
 
+class MapPageState extends State<MapPage> {
+  String campusName;
+  MapPageState(this.campusName);
   Completer<GoogleMapController> _controller = Completer();
+  Visibility mapWindow = Visibility(
+    child: Text(''),
+  );
 
-  double zoomVal = 5.0;
   @override
   Widget build(BuildContext context) {
     List<String> selectedSiteTypes = [];
@@ -39,50 +46,16 @@ class MapPage extends StatelessWidget {
               }),
               title: Text(query.data['ShortName']),
               actions: <Widget>[
-                IconButton(
-                  icon: Icon(FontAwesomeIcons.filter),
-                  onPressed: () {
-                    showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return new AlertDialog(
-                            title: new Text('Select Filters'),
-                            content: Wrap(
-                                children:
-                                    _filterOptions(query, selectedSiteTypes)),
-                            actions: <Widget>[
-                              new FlatButton(
-                                child: Text("Cancel"),
-                                onPressed: () {
-                                  Navigator.of(context).pop(); // dismiss dialog
-                                },
-                              ),
-                              new FlatButton(
-                                child: Text("Continue"),
-                                onPressed: () {
-                                  //apply filters
-                                },
-                              )
-                            ],
-                          );
-                        });
-
-                    //Navigator.push(
-                    // context,
-                    // MaterialPageRoute(builder: (context) => FilterPage()),
-                    //);
-                  },
-                )
+                _filterButton(context, query, selectedSiteTypes),
               ],
             ),
             body: Stack(
               //This could be changed to a different widget type
               children: <Widget>[
                 _buildGoogleMap(context, query, locationsList),
+                mapWindow,
                 _zoomminusfunction(),
                 _zoomplusfunction(),
-                //infopill(),
-                //_buildPlacesContainer(),
               ],
             ),
           );
@@ -141,6 +114,11 @@ class MapPage extends StatelessWidget {
             },
             myLocationButtonEnabled: false,
             markers: Set.from(locationsList),
+            onTap: (argument) {
+              setState(() {
+                mapWindow = Visibility(visible: false, child: Text(''));
+              });
+            },
           ),
         );
       },
@@ -152,50 +130,128 @@ class MapPage extends StatelessWidget {
     final GoogleMapController controller = await _controller.future;
     controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
       target: LatLng(lat, long),
-      //zoom: 15,
+      zoom: 20,
       //tilt: 50.0,
       //bearing: 45.0,
     )));
   }
-}
 
-Marker markerHelper(
-  String longName,
-  String shortName,
-  GeoPoint loc,
-) {
-  return Marker(
-    markerId: MarkerId(shortName),
-    position: LatLng(loc.latitude, loc.longitude),
-    infoWindow: InfoWindow(title: longName),
-    icon: BitmapDescriptor.defaultMarkerWithHue(
-      BitmapDescriptor.hueRed,
-    ),
-    //onTap: (){
-
-    //}
-  );
-}
-
-List<Widget> _filterOptions(
-    DocumentSnapshot query, List<String> selectedSiteTypes) {
-  List<ChoiceChip> filterList = [];
-  List<String> list = [];
-  list = List.from(query.data['buildingTypes']);
-  //List<String> selectedSiteTypes = [];
-//fix this for the filter list and compartmentalize into own method or widget somehow
-  for (String siteType in list) {
-    filterList.add(new ChoiceChip(
-      label: Text(siteType),
-      selected: selectedSiteTypes.contains(siteType),
-      onSelected: (bool selected) {
-        // setState(() {
-        selectedSiteTypes.contains(siteType)
-            ? selectedSiteTypes.remove(siteType)
-            : selectedSiteTypes.add(siteType);
-        // });
+  Marker markerHelper(
+    String longName,
+    String shortName,
+    GeoPoint loc,
+  ) {
+    return Marker(
+      markerId: MarkerId(shortName),
+      position: LatLng(loc.latitude, loc.longitude),
+      icon: BitmapDescriptor.defaultMarkerWithHue(
+        BitmapDescriptor.hueRed,
+      ),
+      onTap: () {
+        setState(() {
+          mapWindow = _mapPopUp(true, longName);
+        });
       },
-    ));
+    );
   }
-  return filterList;
-}
+
+  IconButton _filterButton(context, query, selectedSiteTypes) {
+    return IconButton(
+      icon: Icon(FontAwesomeIcons.filter),
+      onPressed: () {
+        showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return new AlertDialog(
+                title: new Text('Select Filters'),
+                content:
+                    Wrap(children: _filterOptions(query, selectedSiteTypes)),
+                actions: <Widget>[
+                  new FlatButton(
+                    child: Text("Cancel"),
+                    onPressed: () {
+                      Navigator.of(context).pop(); // dismiss dialog
+                    },
+                  ),
+                  new FlatButton(
+                    child: Text("Continue"),
+                    onPressed: () {
+                      //apply filters --> SetState!!!! shown
+                    },
+                  )
+                ],
+              );
+            });
+      },
+    );
+  }
+
+  List<Widget> _filterOptions(
+      DocumentSnapshot query, List<String> selectedSiteTypes) {
+    List<ChoiceChip> filterList = [];
+    List<String> list = [];
+    list = List.from(query.data['buildingTypes']);
+    //List<String> selectedSiteTypes = [];
+//fix this for the filter list and compartmentalize into own method or widget somehow
+    for (String siteType in list) {
+      filterList.add(new ChoiceChip(
+        label: Text(siteType),
+        selected: selectedSiteTypes.contains(siteType),
+        onSelected: (bool selected) {
+          // setState(() {
+          selectedSiteTypes.contains(siteType)
+              ? selectedSiteTypes.remove(siteType)
+              : selectedSiteTypes.add(siteType);
+          // });
+        },
+      ));
+    }
+    return filterList;
+  }
+
+  Widget _mapPopUp(bool vis, String siteName) {
+    if (!vis) return Visibility(child: Scaffold());
+    //Update this here
+    return Visibility(
+        visible: true,
+        child: Container(
+            constraints: BoxConstraints(
+              maxHeight: 50,
+            ),
+            color: Colors.white,
+            child: Row(
+              children: <Widget>[
+                //Image.network(''),
+                Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(left: 0.0),
+                      child: Text(
+                        '$siteName', //Shortname -- Larger Text
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 0.0),
+                      child: Text(
+                        '', //Type -- Smaller Text
+                      ),
+                    ),
+                  ],
+                ),
+                IconButton(
+                  icon: Icon(Icons.arrow_forward),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => InfoPage(
+                                campusName: campusName,
+                                siteName: siteName,
+                              )),
+                    );
+                  },
+                ),
+              ],
+            )));
+  }
+} //class
